3a0d6fe447d0f8df7b3e05d38913137e05670808
Bandana Mahapatra <bmahapatra@tavisca.com>
2017-09-07 20:42:53 +0530

    object transfer over network

---
+++ ServerSide/ServerSide/PersonMan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSide
+{
+    public class PersonMan
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Id { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {FirstName} {LastName}, {PhoneNo}";
+        }
+    }
+}
---
+++ ServerSide/ServerSide/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ServerSide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+            Console.WriteLine("Listening...");
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected");
+
+            NetworkStream stream = client.GetStream();
+            byte[] data = new byte[2048];
+            stream.Read(data, 0, data.Length);
+
+            string dataInString = Encoding.ASCII.GetString(data);
+
+            PersonMan pm = new PersonMan();
+            var field = JsonConvert.DeserializeObject<PersonMan>(dataInString);
+            Console.WriteLine(field.ToString());
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            int datas = stream.Read(buffer, 0, client.ReceiveBufferSize);
+            string ch = Encoding.Unicode.GetString(buffer, 0, datas);
+            Console.WriteLine("mESSAGE RECIEVED " + ch);
+            client.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
